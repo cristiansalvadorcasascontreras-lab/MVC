7519865b7f16453fab631aebdc92538c3ca465bc
cristiansalvadorcasascontreras-lab <cristiansalvadorcasascontreras@gmail.com>
2025-10-29 14:42:09 -0400

    Update principal.py for MVC execution flow
    Refactor principal.py to implement MVC flow.

--- Numero/principal.py
+++ Numero/principal.py
@@ -1,20 +1,14 @@
+# principal.py
 from modelo_numero import Numero
 from vista import Vista_formulario
 from controlador import Controlador
-#zona principal del codigo
-# paso 5
 
-obj_modelo = Numero()
-obj_vista = Vista_formulario()
-obj_controlador = Controlador(obj_vista, obj_modelo)
- 
-""" obj_numero = Numero() 
-obj_numero.set_numero(5)  # paso 6
-print(f"El numero ingresado es: {obj_numero.get_numero()}")  
-# paso 7
-mensaje = obj_numero.validar_par()
-print(f"Resultado: {mensaje}")
-"""
-obj_formuario = Vista_formulario()
-dato_numero = obj_formuario.hacer_campo()
-print(dato_numero)
+# zona principal del código
+if __name__ == "__main__":
+    # paso 1: crear objetos de cada capa
+    obj_modelo = Numero()
+    obj_vista = Vista_formulario()
+    obj_controlador = Controlador(obj_vista, obj_modelo)
+
+    # paso 2: ejecutar el flujo MVC
+    obj_controlador.ejecutar_programa()
