fb0517ab5f88041ebeaf046e98acab30a271c073
cristiansalvadorcasascontreras-lab <cristiansalvadorcasascontreras@gmail.com>
2025-10-29 14:41:10 -0400

    Update vista.py

--- Numero/vista.py
+++ Numero/vista.py
@@ -1,14 +1,15 @@
+# vista.py
 class Vista_formulario:
     def __init__(self):
-        self.nombre_modelo = "validar numeros"
-        self.texto_pregunta = "digite un numero: "
-        self.campo_numero = ""
+        self.nombre_modelo = "=== VALIDAR NÚMEROS ==="
+        self.texto_pregunta = "Digite un número: "
+        self.campo_numero = 0
 
     def hacer_campo(self):
         print(self.nombre_modelo)
         print(self.texto_pregunta)
         self.campo_numero = int(input())
         return self.campo_numero
+
     def imprimir_resultado(self, dato_mensaje):
-        print(dato_mensaje)
-        
+        print("Resultado:", dato_mensaje)
